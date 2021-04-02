1758b9448829fbbadf4c5aed5d8e0247c03bec1d
Sourcery AI <>
2021-04-02 02:43:10 +0000

    'Refactored by Sourcery'

--- python/check-plugins.py
+++ python/check-plugins.py
@@ -6,13 +6,13 @@ VST = Path('/Library/Audio/Plug-Ins/VST')
 AU = Path('/Library/Audio/Plug-Ins/Components')
 
 
-vsts = set(i.stem for i in VST.iterdir())
-aus = set(i.stem for i in AU.iterdir())
+vsts = {i.stem for i in VST.iterdir()}
+aus = {i.stem for i in AU.iterdir()}
 
 
 def rm_puremagnetik_vsts():
     puremagnetik = Path('~/Downloads/music-tech/plugins/au-vst/puremagnetik')
-    ps = set(i.stem for i in puremagnetik.iterdir())
+    ps = {i.stem for i in puremagnetik.iterdir()}
 
     for i in ps.intersection(vsts):
         target = VST / f'{i}.vst'
